bd5044b6394825a0a200c6c3c0c95b689ef675d1
chadperson <chadperson@gmail.com>
2016-11-07 09:37:06 -0700

    adding animate.css WIP

---
+++ _site/js/theme.js
@@ -0,0 +1,14 @@
+(function(){
+
+// animate icon/img appearance on scroll
+$(window).scroll(function() {
+  $('.animateEntry').each(function(){
+  var imagePos = $(this).offset().top;
+
+  var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+200) {
+      $(this).addClass("animated fadeInUp");
+    }
+  });
+});
+}
