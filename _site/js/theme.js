c723ec3c3a59e98cd31b822ae033f75b78a36847
chadperson <chadperson@gmail.com>
2016-11-07 12:09:49 -0700

    cm site updates

--- _site/js/theme.js
+++ _site/js/theme.js
@@ -1,14 +1,27 @@
-(function(){
-
 // animate icon/img appearance on scroll
 $(window).scroll(function() {
   $('.animateEntry').each(function(){
   var imagePos = $(this).offset().top;
 
   var topOfWindow = $(window).scrollTop();
-    if (imagePos < topOfWindow+200) {
+    if (imagePos < topOfWindow+480) {
       $(this).addClass("animated fadeInUp");
     }
   });
+  $('.animateEntryLeft').each(function(){
+  var imagePos = $(this).offset().top;
+
+  var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+480) {
+      $(this).addClass("animated fadeInLeft");
+    }
+  });
+  $('.animateEntryRight').each(function(){
+  var imagePos = $(this).offset().top;
+
+  var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+480) {
+      $(this).addClass("animated fadeInRight");
+    }
+  });
 });
-}
--- js/theme.js
+++ js/theme.js
@@ -4,8 +4,24 @@ $(window).scroll(function() {
   var imagePos = $(this).offset().top;
 
   var topOfWindow = $(window).scrollTop();
-    if (imagePos < topOfWindow+200) {
+    if (imagePos < topOfWindow+480) {
       $(this).addClass("animated fadeInUp");
+    }
+  });
+  $('.animateEntryLeft').each(function(){
+  var imagePos = $(this).offset().top;
+
+  var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+480) {
+      $(this).addClass("animated fadeInLeft");
+    }
+  });
+  $('.animateEntryRight').each(function(){
+  var imagePos = $(this).offset().top;
+
+  var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+480) {
+      $(this).addClass("animated fadeInRight");
     }
   });
 });
